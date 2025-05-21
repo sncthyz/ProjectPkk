29694607654b4cea9c18bd50e7878751abca96f9
InMochi <ooer379@gmail.com>
2025-05-21 22:57:50 +0700

    nih kal

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,23 +1,22 @@
 @extends('components.layouts') {{-- Ganti kalau kamu pakai layout lain --}}
 
 @section('content')
-<div class="container main-content-center">
-    <div class="text-center mb-9">
+    <div class="text-center mb-3">
         <h2 class="mb-12">Daftar Post</h2>
         <a href="{{ route('posts.create') }}" class="btn btn-primary">+ Tambah Post</a>
     </div>
-<div class="row"></div>
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+    <div class="row"></div>
+    {{-- @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif --}}
 
     <div class="row g-3">
         @forelse($posts as $post)
             <div class="col-md-4">
                 <div class="card h-100 shadow-sm">
-                    @if($post->imagePath)
+                    @if ($post->imagePath)
                         <img src="{{ asset('storage/' . $post->imagePath) }}" class="card-img-top" alt="Gambar Post">
                     @endif
                     <div class="card-body d-flex flex-column">
@@ -29,7 +28,8 @@
                         <div class="d-flex justify-content-between gap-1">
                             <a href="{{ route('posts.show', $post) }}" class="btn btn-sm btn-info w-100">Lihat</a>
                             <a href="{{ route('posts.edit', $post) }}" class="btn btn-sm btn-warning w-100">Edit</a>
-                            <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('Yakin hapus?')" class="w-100">
+                            <form action="{{ route('posts.destroy', $post) }}" method="POST"
+                                onsubmit="return confirm('Yakin hapus?')" class="w-100">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-sm btn-danger w-100">Hapus</button>
@@ -46,5 +46,4 @@
             </div>
         @endforelse
     </div>
-</div>
 @endsection
